9c61d10005415327f76ed2f7cd95a908f02f9d50
Jeston <JestonBlu@gmail.com>
2016-05-07 12:54:48 -0500

    added apt sript

---
+++ scripts/apt.sh
@@ -0,0 +1,2 @@
+## fix missing key with apt-get update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553
--- scripts/flash.sh
+++ scripts/flash.sh
@@ -2,6 +2,6 @@ ps aux | grep flash
 
 cd /proc/3324/fd
 
-ls -hal | grep Flash
+ls -hal | grep Fresh
 
 cp 17 ~/videoname.flv
